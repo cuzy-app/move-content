b7851385162eefa11ae76d73034d6d0a6b08c369
Marc Farré <contact@marc.fun>
2022-11-21 23:33:08 +0100

    update

--- jobs/MoveUserContentJob.php
+++ jobs/MoveUserContentJob.php
@@ -8,6 +8,10 @@
 
 namespace humhub\modules\moveContent\jobs;
 
+use humhub\modules\activity\models\Activity;
+use humhub\modules\cfiles\models\File;
+use humhub\modules\cfiles\models\Folder;
+use humhub\modules\content\components\ContentActiveRecord;
 use humhub\modules\content\models\Content;
 use humhub\modules\queue\ActiveJob;
 use humhub\modules\user\models\User;
@@ -34,6 +38,26 @@ class MoveUserContentJob extends ActiveJob implements RetryableJobInterface
     public $moveProfileContent = false;
 
     /**
+     * @var Content
+     */
+    protected $_content;
+
+    /**
+     * @var Content
+     */
+    protected $_model;
+
+    /**
+     * @var ContentActiveRecord
+     */
+    protected $_nbContentMoved = 0;
+
+    /**
+     * @var array
+     */
+    protected $_errors = [];
+
+    /**
      * @inhertidoc
      * @var int maximum 1 hour
      */
@@ -55,35 +79,68 @@ class MoveUserContentJob extends ActiveJob implements RetryableJobInterface
             ->where(['created_by' => $sourceUser->id])
             ->orWhere(['contentcontainer_id' => $sourceUser->contentcontainer_id]);
 
-        $nbContentMoved = 0;
-        $errors = [];
         /** @var Content $content */
         foreach ($contentQuery->each() as $content) {
-            $content->created_by = $targetUser->id;
-            if ($content->container instanceof User) {
-                if (!$this->moveProfileContent) {
-                    continue;
-                }
-                $content->contentcontainer_id = $targetUser->contentcontainer_id;
+
+            // Get and check Content and Model
+            $this->_content = $content;
+            try {
+                $this->_model = $this->_content->getModel();
+            } catch (IntegrityException $e) {
+                echo '111';
+                continue;
             }
-            if ($content->save()) {
-                $nbContentMoved++;
-                try {
-                    $model = $content->getPolymorphicRelation();
-                    if (!empty($model->created_by)) {
-                        $model->created_by = $targetUser->id;
-                        $model->save();
-                    }
-                } catch (IntegrityException $e) {
+            if (
+                !$this->_model
+                || $this->_model instanceof Activity
+            ) {
+                continue;
+            }
+
+            // The content is in the user profile
+            if ($this->_content->container instanceof User) {
+                if (
+                    $this->moveProfileContent
+                    && !$this->_model instanceof File
+                    && !$this->_model instanceof Folder
+                    && $this->_content->container->moduleManager->isEnabled($this->_model->getModuleId())
+                ) {
+                    $this->_content->contentcontainer_id = $targetUser->contentcontainer_id;
+                    $this->changeContentOwner($targetUser);
                 }
-            } else {
-                $errors[] = implode(' ', $model->getErrorSummary(true));
+            } // The content is global or in a space
+            else {
+                $this->changeContentOwner($targetUser);
             }
         }
 
-        Yii::warning($nbContentMoved . ' contents of user "' . $sourceUser->username . '" have been transferred to user "' . $targetUser->username . '"', 'move-content');
-        if ($errors) {
-            Yii::error('Errors while transferring content from user "' . $sourceUser->username . '" to user "' . $targetUser->username . '": ' . implode(' | ', $errors), 'move-content');
+        // Log result
+        Yii::warning($this->_nbContentMoved . ' contents of user "' . $sourceUser->username . '" have been transferred to user "' . $targetUser->username . '"', 'move-content');
+        if ($this->_errors) {
+            Yii::error('Errors while transferring content from user "' . $sourceUser->username . '" to user "' . $targetUser->username . '": ' . implode(' | ', $this->_errors), 'move-content');
+        }
+    }
+
+    /**
+     * @param User $targetUser
+     * @return void
+     */
+    protected function changeContentOwner(User $targetUser)
+    {
+        $this->_content->created_by = $targetUser->id;
+
+        if ($this->_content->save()) {
+            if (isset($this->_model->created_by)) {
+                $this->_model->created_by = $targetUser->id;
+                $scenarios = $this->_model->scenarios();
+                $scenario = $this->_model->getScenario();
+                if (isset($scenarios[$scenario])) {
+                    $this->_model->save();
+                }
+            }
+            $this->_nbContentMoved++;
+        } else {
+            $this->_errors[] = 'Content ID ' . $this->_content->id . ': ' . implode(' ', $this->_content->getErrorSummary(true));
         }
     }
 
