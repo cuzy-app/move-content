a5789f976c11ba228679daf2203a433195511579
marc-farre <23310825+marc-farre@users.noreply.github.com>
2025-12-12 05:24:14 +0000

    [create-pull-request] automated change

--- config.php
+++ config.php
@@ -19,12 +19,12 @@ return [
         [
             'class' => UserMenu::class,
             'event' => UserMenu::EVENT_INIT,
-            'callback' => [Events::class, 'onAdminUserMenuInit'],
+            'callback' => Events::onAdminUserMenuInit(...),
         ],
         [
             'class' => SpaceMenu::class,
             'event' => SpaceMenu::EVENT_INIT,
-            'callback' => [Events::class, 'onAdminSpaceMenuInit'],
+            'callback' => Events::onAdminSpaceMenuInit(...),
         ],
     ],
 ];
